7d6d819e0d1f2919fff2598617d9d4a2fc4522fd
David Norris <norris@vm-u22lts.fritz.box>
2022-11-08 10:08:13 +0100

    Working firmware; delays set low for testing.

--- main/main.cpp
+++ main/main.cpp
@@ -1,47 +1,163 @@
-/*
- * train-barrier-firmware
- * ESP32 firmware for train barrier servo motor and light controller
- * Copyright (C) 2022 David Norris & Alexander Norris
- */
+//
+// train-barrier-firmware
+// ESP32 firmware for train barrier servo motor and light controller
+// Copyright (C) 2022 David Norris & Alexander Norris
+//
 
-#include <stdio.h>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
 
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
-
+#include <esp++/errors.hpp>
+#include <esp++/gpio.hpp>
 #include <esp++/main.hpp>
+#include <freertos++/time.hpp>
 
-extern "C" void app_main(void)
+#include <driver/mcpwm.h>
+
+using namespace std::chrono_literals;
+
+static constexpr int            MOTOR_GPIO = 27;
+static constexpr int            SWITCH_GPIO = 25;
+static constexpr int            YELLOW_GPIO = 32;
+static constexpr int            RED_GPIO = 33;
+static constexpr double         ANGLE_VERTICAL = 0.0;
+static constexpr double         ANGLE_HORIZONTAL = 90.0;
+static constexpr auto           BLINK_INTERVAL = 750ms;
+static constexpr std::uint32_t  PWM_FREQUENCY = 50;
+
+static constexpr auto           ON_DELAY = 250ms;
+static constexpr auto           YELLOW_DURATION = 250ms;
+static constexpr auto           GATE_DELAY = 500ms;
+static constexpr auto           TRAVEL_TIME = 5s;
+
+static constexpr double         VERTICAL_PERIOD = 2050.0;
+static constexpr double         HORIZONTAL_PERIOD = 1050.0;
+
+static constexpr double ANGLE_TO_PERIOD(double angle)
 {
-    esp::safe_main([]()
+    double f = (angle - ANGLE_HORIZONTAL) / (ANGLE_VERTICAL - ANGLE_HORIZONTAL);
+    return HORIZONTAL_PERIOD + f * (VERTICAL_PERIOD - HORIZONTAL_PERIOD);
+}
+
+mcpwm_config_t pwm_config =
+{
+    .frequency      = PWM_FREQUENCY,
+    .cmpr_a         = 0,
+    .cmpr_b         = 0,
+    .duty_mode      = MCPWM_DUTY_MODE_0,
+    .counter_mode   = MCPWM_UP_COUNTER,
+};
+
+ESPPP_MAIN()
+{
+    typedef std::chrono::duration<double> duration;
+
+    double current_angle = ANGLE_VERTICAL;
+    auto last_loop_time = std::chrono::steady_clock::now();
+    auto last_off_time = last_loop_time;
+
+    esp::gpio::set(YELLOW_GPIO, false);
+    esp::gpio::set(RED_GPIO, false);
+    esp::gpio::configure({ YELLOW_GPIO, RED_GPIO }, GPIO_MODE_OUTPUT);
+    esp::gpio::configure(SWITCH_GPIO, GPIO_MODE_INPUT, true);
+
+    esp::check(
+        mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, MOTOR_GPIO),
+        "failed to initialize motor gpio");
+
+    esp::check(
+        mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config),
+        "failed to initialize motor control");
+
+    while (true)
     {
-        printf("Hello world!\n");
+        auto now = std::chrono::steady_clock::now();
+        auto dt = now - last_loop_time;
+        bool switch_on = !esp::gpio::get(SWITCH_GPIO);
+        double target_angle = current_angle;
 
-        /* Print chip information */
-        esp_chip_info_t chip_info;
-        esp_chip_info(&chip_info);
-        printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-                CONFIG_IDF_TARGET,
-                chip_info.cores,
-                (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-                (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+        if (switch_on)
+        {
+            auto elapsed = now - last_off_time;
 
-        printf("silicon revision %d, ", chip_info.revision);
+            if (elapsed < ON_DELAY)
+            {
+                esp::gpio::set(YELLOW_GPIO, false);
+                esp::gpio::set(RED_GPIO, false);
+                target_angle = ANGLE_VERTICAL;
+            }
 
-        printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-                (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+            else if (elapsed < ON_DELAY + YELLOW_DURATION)
+            {
+                esp::gpio::set(YELLOW_GPIO, true);
+                esp::gpio::set(RED_GPIO, false);
+                target_angle = ANGLE_VERTICAL;
+            }
 
-        printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+            else if (elapsed < ON_DELAY + YELLOW_DURATION + GATE_DELAY)
+            {
+                esp::gpio::set(YELLOW_GPIO, false);
+                esp::gpio::set(RED_GPIO, true);
+                target_angle = ANGLE_VERTICAL;
+            }
 
-        for (int i = 10; i >= 0; i--) {
-            printf("Restarting in %d seconds...\n", i);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            else if (elapsed < ON_DELAY + YELLOW_DURATION + GATE_DELAY + TRAVEL_TIME)
+            {
+                esp::gpio::set(YELLOW_GPIO, false);
+                esp::gpio::set(RED_GPIO, true);
+
+                double f =
+                    std::chrono::duration_cast<duration>(elapsed - ON_DELAY - YELLOW_DURATION - GATE_DELAY) /
+                    std::chrono::duration_cast<duration>(TRAVEL_TIME);
+
+                target_angle = ANGLE_VERTICAL + f * (ANGLE_HORIZONTAL - ANGLE_VERTICAL);
+            }
+
+            else
+            {
+                esp::gpio::set(YELLOW_GPIO, false);
+                esp::gpio::set(RED_GPIO, true);
+                target_angle = ANGLE_HORIZONTAL;
+            }
+
         }
-        printf("Restarting now.\n");
-        fflush(stdout);
-        esp_restart();
-    });
+
+        else
+        {
+            last_off_time = now;
+
+            target_angle = ANGLE_VERTICAL;
+
+            esp::gpio::set(YELLOW_GPIO, false);
+            esp::gpio::set(RED_GPIO, current_angle != target_angle);
+        }
+
+        if (current_angle < target_angle)
+        {
+            current_angle += std::chrono::duration_cast<duration>(dt) *
+                std::abs(ANGLE_HORIZONTAL - ANGLE_VERTICAL)
+                / std::chrono::duration_cast<duration>(TRAVEL_TIME);
+
+            if (current_angle > target_angle)
+                current_angle = target_angle;
+        }
+
+        else if (current_angle > target_angle)
+        {
+            current_angle -= std::chrono::duration_cast<duration>(dt) *
+                std::abs(ANGLE_HORIZONTAL - ANGLE_VERTICAL)
+                / std::chrono::duration_cast<duration>(TRAVEL_TIME);
+
+            if (current_angle < target_angle)
+                current_angle = target_angle;
+        }
+
+        mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A,
+            ANGLE_TO_PERIOD(current_angle));
+
+        last_loop_time = now;
+
+        freertos::sleep(10ms);
+    }
 }
